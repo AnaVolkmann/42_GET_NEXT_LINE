1301b910d98f4de87b816eebdad3cccec1ed4117
Ana Laura Da Silva Volkmann <ana-lda-@c1r8s8.42porto.com>
2024-05-17 11:16:44 +0100

    lib

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-lda- <ana-lda-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 11:03:21 by ana-lda-          #+#    #+#             */
+/*   Updated: 2024/05/17 11:07:51 by ana-lda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+#endif

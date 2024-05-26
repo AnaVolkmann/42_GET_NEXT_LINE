839a3744f124d38c16733b012ed96de49c5efc2b
Ana Laura Da Silva Volkmann <ana-lda-@c1r2s1.42porto.com>
2024-05-26 18:11:49 +0100

    pronto

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ana-lda- <ana-lda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 19:23:06 by ana-lda-          #+#    #+#             */
-/*   Updated: 2024/05/25 22:19:36 by ana-lda-         ###   ########.fr       */
+/*   Updated: 2024/05/26 18:09:39 by ana-lda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ GNL gerencia vários arquivos abertos ao mesmo tempo.
 o restante do buffer que ainda não foi processado para cada fd.*/
 char	*get_next_line(int fd)
 {
-	static char	*laststr[10];
+	static char	*laststr[1024];
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
@@ -107,68 +107,40 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = ft_getline(laststr[fd]);
 	laststr[fd] = ft_new_left_str(laststr[fd]);
-	free(laststr[fd]);
 	return (line);
 }
-/*  #include <stdio.h>
+/* #include <stdio.h>
 
-int	main(void)
+int  main(void)
 {
-	int		fd;
-    int		fd1;
-    int		fd2;
-	char	*str;
+	int     fd1;
+	int     fd2;
+	int     fd3;
+	char    *line;
 
-	fd = open("texto.txt", O_RDONLY);
-    fd1 = open("texto2.txt", O_RDONLY);
-    fd2 = open("texto3.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error\n");
-		return (0);
-	}
-	str = get_next_line(fd);
-	printf("line 1 -> %s\n", str);
-	free(str);
-    str = get_next_line(fd1);
-    printf("line 2 -> %s\n", str);
-	free(str);
-	str = get_next_line(fd2);
-	printf("line 3 -> %s\n", str);
-	free(str);
-	return (0);
-} */
-
-/* void handle_file(const char *filename, int line_number) 
-{
-    int fd;
-
-	fd = open(filename, O_RDONLY);
-    if (fd == -1) 
-	{
-        printf("Error opening %s\n", filename);
-        return;
-    }
-
-    char *str = get_next_line(fd);
-    if (str) 
-	{
-        printf("line %d -> %s\n", line_number, str);
-		free(str);
-    }
-	else 
-	{
-        printf("Error reading line %d from %s\n", line_number, filename);
-    }
-
-    close(fd);
-}
-
-int main(void)
-{
-	handle_file("texto.txt", 1);
-	handle_file("texto2.txt", 1);
-	handle_file("texto3.txt", 1);
-
+	fd1 = open("texto.txt", O_RDONLY);
+	fd2 = open("texto2.txt", O_RDONLY);
+	fd3 = open("texto3.txt", O_RDONLY);
+	line = get_next_line(fd1);
+		printf("line 1->%s\n", line);
+		free (line);
+	line = get_next_line(fd2);
+		printf("line 2->%s\n", line);
+		free (line);
+	line = get_next_line(fd3);
+		printf("line 3->%s\n", line);
+		free (line);
+	line = get_next_line(fd1);
+		printf("line 4->%s\n", line);
+		free (line);
+	line = get_next_line(fd2);
+		printf("line 5->%s\n", line);
+		free (line);
+	line = get_next_line(fd3);
+		printf("line 6->%s\n", line);
+		free (line);
+	close (fd1);
+	close (fd2);
+	close (fd3);
 	return (0);
 } */
